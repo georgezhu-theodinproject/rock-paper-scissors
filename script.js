384319f62aec8ab0778c5c5beb83cf766730eb91
George Zhu <georgezhu211@gmail.com>
2025-11-20 19:10:29 -0600

    Update UI

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll("button");
+const status = document.querySelector(".status");
+const results = document.querySelector(".results");
+const humanScoreDiv = document.querySelector(".human-score");
+const computerScoreDiv = document.querySelector(".computer-score");
+
 function getComputerChoice() {
   switch (getRandomInt(3)) {
     case 0:
@@ -9,63 +18,35 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt("Rock, paper, or scissors?").toLowerCase();
-}
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function playRound(human, computer) {
-  if (human == computer) {
-    results.textContent = "You draw!";
-    console.log("You draw!");
-    results.style.color = "black";
-    return;
+  if (human === computer) {
+    results.textContent = "It's a tie!";
   } else if (
     (human === "rock" && computer === "scissors") ||
     (human === "paper" && computer === "rock") ||
     (human === "scissors" && computer === "paper")
   ) {
-    results.textContent = `You win! ${human} beats ${computer}`;
-    results.style.color = "green";
-
+    results.textContent = `You win!`;
     humanScore++;
   } else {
-    results.textContent = `You lose! ${computer} beats ${human}`;
-    results.style.color = "red";
-
+    results.textContent = `You lose!`;
     computerScore++;
   }
 }
 
-function playGame() {
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-}
-
-const buttons = document.querySelectorAll("button");
-const results = document.querySelector(".results");
-const humanScoreDiv = document.querySelector(".player-score");
-const computerScoreDiv = document.querySelector(".computer-score");
-
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.id);
     const humanChoice = button.id;
-    playRound(button.id, getComputerChoice());
+    const computerChoice = getComputerChoice();
+
+    status.textContent = `You chose ${humanChoice}. Computer chose ${computerChoice}.`;
+    playRound(humanChoice, computerChoice);
+
     humanScoreDiv.textContent = humanScore;
     computerScoreDiv.textContent = computerScore;
   });
 });
-
-let humanScore = 0;
-let computerScore = 0;
-
-// playGame();
-
-// console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
